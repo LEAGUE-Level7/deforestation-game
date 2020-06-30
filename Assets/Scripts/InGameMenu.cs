7602e285b5fc899a39209e5bdff65f53b59906b4
Ryguy-1 <Ryguy-1@users.noreply.github.com>
2020-06-29 19:56:08 -0700

    Initial commit

---
+++ Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] GameObject menu;
+
+
+    private void Update()
+    {
+       
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Ran");
+            revealMenu();
+        }
+    }
+
+
+    private void revealMenu()
+    {
+        if (menu != null) 
+            menu.SetActive(!menu.activeSelf);
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/MenuNavigation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuNavigation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        Debug.Log("Game Started");
+    }
+
+    // Update is called once per frame
+     public void quitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+        Debug.Log("Quit Game");
+#endif     
+    }
+}
