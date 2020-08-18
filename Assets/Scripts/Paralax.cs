c2187af29e12f01aa0703908c7da6bff95bfa920
KevinWh0 <45321184+KevinWh0@users.noreply.github.com>
2020-08-17 20:02:14 -0700

    Added Win Screen Scene and a little paralax script. Also added paralax to main menu

---
+++ Assets/Scripts/Paralax.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+
+    [SerializeField] GameObject ParalaxImg;
+    [SerializeField] int amount;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        ParalaxImg.transform.position = new Vector3((mousePos.x/ amount) + 600, (mousePos.y/ amount) + 300, 0);
+    }
+}
---
+++ Assets/Scripts/WinScreenToMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreenToMenu : MonoBehaviour
+{
+    public void returnToMenu()
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("Returning To The Menu");
+    }
+}
+
+
