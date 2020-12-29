1c572e1464f0ea4aae4bbdf471dd36fafa3d89ab
KevinWh0 <45321184+KevinWh0@users.noreply.github.com>
2020-12-28 19:04:03 -0800

    Fixed Button multiplying glitch (#41)
    * Fixed Button multiplying glitch
    
    * 2 Fixes. Wrong values on first button. same question wont be picked twice.

--- Assets/Scripts/QuestionButtonAmt.cs
+++ Assets/Scripts/QuestionButtonAmt.cs
@@ -22,8 +22,37 @@ public class QuestionButtonAmt : MonoBehaviour
     int selectedQuestionID;
 
 
+    public static int lastQuestionID = -1;
+
     void Start()
     {
+        setup();
+
+    }
+
+
+    public void reset()
+    {
+        for (int i = 2; i < 10; i++)
+        {
+            GameObject go = GameObject.Find("Choice " + i);
+
+            try
+            {
+                Destroy(go.gameObject);
+            }
+            catch (Exception e) { }
+                
+
+
+        }
+        button.GetComponent<Button>().onClick.RemoveAllListeners();
+        setup();
+
+    }
+    public void setup()
+    {
+        print("e");
         string fileName = Path.Combine(Application.dataPath, quesFile);
 
         //pass in the original text object and then the question as a string
@@ -32,7 +61,7 @@ public class QuestionButtonAmt : MonoBehaviour
         using (StreamReader r = new StreamReader(fileName))
         {
 
-            
+
             string json = r.ReadToEnd();
             QuestionLists items = JsonUtility.FromJson<QuestionLists>(json);
             /*for (int i = 0; i < items.Questions.Length; i++)
@@ -42,7 +71,10 @@ public class QuestionButtonAmt : MonoBehaviour
             }*/
 
             selectedQuestionID = (int)Math.Floor((float)(new System.Random().NextDouble()) * items.Questions.Length);
-
+            while(lastQuestionID == selectedQuestionID)
+            {
+                selectedQuestionID = (int)Math.Floor((float)(new System.Random().NextDouble()) * items.Questions.Length);
+            }
 
             questionText.text = items.Questions[selectedQuestionID].Question;
 
@@ -51,9 +83,9 @@ public class QuestionButtonAmt : MonoBehaviour
             //pass in the original button and then a string array of the answers
 
             string[] questions = new String[items.Questions[selectedQuestionID].Answers.Length];
-            string[,] types = new String[items.Questions[selectedQuestionID].Answers.Length,8];
-            int[,] max = new int[items.Questions[selectedQuestionID].Answers.Length,8];
-            int[,] min = new int[items.Questions[selectedQuestionID].Answers.Length,8];
+            string[,] types = new String[items.Questions[selectedQuestionID].Answers.Length, 8];
+            int[,] max = new int[items.Questions[selectedQuestionID].Answers.Length, 8];
+            int[,] min = new int[items.Questions[selectedQuestionID].Answers.Length, 8];
 
             for (int i = 0; i < items.Questions[selectedQuestionID].Answers.Length; i++)
             {
@@ -61,9 +93,9 @@ public class QuestionButtonAmt : MonoBehaviour
 
                 for (int j = 0; j < items.Questions[selectedQuestionID].Answers[i].Effects.Length; j++)
                 {
-                    types[i,j] = items.Questions[selectedQuestionID].Answers[i].Effects[j].Type;
-                    min[i,j] = items.Questions[selectedQuestionID].Answers[i].Effects[j].MinEffect;
-                    max[i,j] = items.Questions[selectedQuestionID].Answers[i].Effects[j].MaxEffect;
+                    types[i, j] = items.Questions[selectedQuestionID].Answers[i].Effects[j].Type;
+                    min[i, j] = items.Questions[selectedQuestionID].Answers[i].Effects[j].MinEffect;
+                    max[i, j] = items.Questions[selectedQuestionID].Answers[i].Effects[j].MaxEffect;
                 }
                 //types[i] = items.Questions[selectedQuestionID].Answers[i].Effects[0].Type;
                 //min[i] = items.Questions[selectedQuestionID].Answers[i].Effects[0].MinEffect;
@@ -73,33 +105,34 @@ public class QuestionButtonAmt : MonoBehaviour
             }
 
             createButtons(button, questions, types, max, min);
+            lastQuestionID = selectedQuestionID;
         }
 
     }
 
 
-   /* void setAnswers(Text answerText, String answer)
-    {
-        string fileName = Path.Combine(Application.dataPath, quesFile);
-        using (StreamReader r = new StreamReader(fileName))
-        {
-            string json = r.ReadToEnd();
-            QuestionLists items = JsonUtility.FromJson<QuestionLists>(json);
-            for (int i = 0; i <; i++)
-            {
-                answer = items.Questions[i].Answers[i].AnswerText;
-                Debug.Log(items.Questions[i].Answers[i].AnswerText);
-            }
-        }
-        answerText.text = answer;
+    /* void setAnswers(Text answerText, String answer)
+     {
+         string fileName = Path.Combine(Application.dataPath, quesFile);
+         using (StreamReader r = new StreamReader(fileName))
+         {
+             string json = r.ReadToEnd();
+             QuestionLists items = JsonUtility.FromJson<QuestionLists>(json);
+             for (int i = 0; i <; i++)
+             {
+                 answer = items.Questions[i].Answers[i].AnswerText;
+                 Debug.Log(items.Questions[i].Answers[i].AnswerText);
+             }
+         }
+         answerText.text = answer;
 
-    }*/
+     }*/
     void onCLickChoice(GameObject originalButton, int n)
     {
         Debug.Log("clicked");
     }
 
-    void createButtons(GameObject originalButton, String[] answers, String[,] types, int[,] max, int[,] min)
+void createButtons(GameObject originalButton, String[] answers, String[,] types, int[,] max, int[,] min)
     {
         float originalX = button.transform.position.x;
 
@@ -108,13 +141,13 @@ public class QuestionButtonAmt : MonoBehaviour
 
         ButtonText = button.GetComponentInChildren(typeof(Text)) as Text;
         ButtonText.text = answers[0];
+        
         originalButton.GetComponent<Button>().onClick.AddListener(delegate {
-            for(int i = 0; i < 8; i++)
+            for (int l = 0; l < 8; l++)
             {
-
-                updateResources(types[0, i], (int)UnityEngine.Random.Range(min[0, i], max[0, i]));
-                
+                updateResources(types[0, l], (int)UnityEngine.Random.Range(min[0, l], max[0, l]));
             }
+            reset();
         });
 
         for (int i = 1; i < answers.Length; i++)
@@ -133,14 +166,13 @@ public class QuestionButtonAmt : MonoBehaviour
                 {
                     updateResources(types[j,l], (int)UnityEngine.Random.Range(min[j,l], max[j,l]));
                 }
-                //for (int l = 0; l < min[0].Length; l++)
-                //{
-                //   updateResources(types[j][l], (int)UnityEngine.Random.Range(min[j][l], max[j][l]));
-                //}
-                //updateResources(types[j], (int) UnityEngine.Random.Range(min[j], max[j]));
+                reset();
             });
         }
     }
+
+
+
     public void updateResources(String r, int amount)
     {
         switch (r) {
